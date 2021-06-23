d8ba4998f77ce1c766833a4f536ae2ffa7865b27
Developer-Nasim <unique0coder@gmail.com>
2021-06-24 03:39:26 +0600

    all upload

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+   
+
+
+  /* magnificPopup img view */
+  $(".play-btn").magnificPopup({
+    items: { src: 'https://www.youtube.com/watch?v=7eo8XpT4CmM' },
+    type: 'iframe',
+    iframe: {
+          markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                  '</div>', 
+          patterns: {
+              youtube: {
+                  index: 'youtube.com/', 
+                  id: 'v=', 
+                  src: '//www.youtube.com/embed/%id%?autoplay=1' 
+              }
+           },
+           srcAction: 'iframe_src', 
+       }
+  });
+
+
+ 
+  // owlCarousel
+  $(".brand-active").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 6
+      }
+    }
+  });
+ 
+})(jQuery);
